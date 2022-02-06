caf3c659e8c9e92fea5d43e2550110c1213b2d27
Robbo76 <steve_robinson@icloud.com>
2022-02-06 11:28:17 +0000

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-console.log("connected!")
+function runGame(params) {
+    
+}
+
+function checkAnswer(params) {
+    
+}
+
+function calculateCorrectAnswer(params) {
+    name
+}
+
+function incrementScore(params) {
+    name
+}
+
+function incrementWrongAnswer(params) {
+    name
+}
+
+function displayAdditionQuestion(params) {
+    name
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(params) {
+    
+}
